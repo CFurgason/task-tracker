a85ae863b6503057d6233b110b8e5554d13e510a
CFurgason <courtney@autoshopcallbacks.net>
2026-07-07 14:00:46 -0500

    Add files via upload

--- google-sheets-config.js
+++ google-sheets-config.js
@@ -1,4 +1,4 @@
 // Paste the Google Sheets CSV publish/export URL between the quotes.
 // You can also test a URL temporarily with: index.html?sheetCsv=https://...
-window.TASK_TRACKER_GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/1-l0LXj6Mt7e73YdWcLGqRRLwR34GtrbnBWo6e-O3_qM/edit?gid=0#gid=0";
+window.TASK_TRACKER_GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/1-l0LXj6Mt7e73YdWcLGqRRLwR34GtrbnBWo6e-O3_qM/edit?usp=sharing";
 
