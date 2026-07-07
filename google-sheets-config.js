022f2bc85f9bf61965e2f21451cf71a3b0db4165
CFurgason <courtney@autoshopcallbacks.net>
2026-07-07 13:38:54 -0500

    Add files via upload

--- google-sheets-config.js
+++ google-sheets-config.js
@@ -1,3 +1,4 @@
 // Paste the Google Sheets CSV publish/export URL between the quotes.
 // You can also test a URL temporarily with: index.html?sheetCsv=https://...
-window.TASK_TRACKER_GOOGLE_SHEET_CSV_URL = "";
+window.window.TASK_TRACKER_GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/YOUR_PUBLISHED_SHEET_ID/pub?gid=0&single=true&output=csv";
+
